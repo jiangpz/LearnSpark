598789c1d5a6904169333ed514e5df51642d5416
Jpz <Jiangpeizhao@outlook.com>
2016-06-21 18:01:50 +0800

    lambda wordcount

--- src/main/java/SparkLearn/SparkTest/WordCountLambdas.java
+++ src/main/java/SparkLearn/SparkTest/WordCountLambdas.java
@@ -8,15 +8,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 
 import scala.Tuple2;
 
-public class WordCount {
+public class WordCountLambdas {
 	private static final Pattern SPACE = Pattern.compile("data");
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("1-----------------------------");
@@ -25,28 +22,9 @@ public class WordCount {
 		JavaRDD<String> lines = ctx.textFile("D:\\behavior.txt", 1);
 
 		System.out.println("2-----------------------------");
-		JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-			private static final long serialVersionUID = 1L;
-			public Iterable<String> call(String s) {
-				return Arrays.asList(SPACE.split(s));
-			}
-		});
-
-		System.out.println("3-----------------------------");
-		JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-			private static final long serialVersionUID = 1L;
-			public Tuple2<String, Integer> call(String s) {
-				return new Tuple2<String, Integer>(s, 1);
-			}
-		});
-
-		System.out.println("4-----------------------------");
-		JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-			private static final long serialVersionUID = 1L;
-			public Integer call(Integer i1, Integer i2) {
-				return i1 + i2;
-			}
-		});
+		JavaPairRDD<String, Integer> counts = lines.flatMap(s -> Arrays.asList(SPACE.split(s)))
+												   .mapToPair(s -> new Tuple2<String, Integer>(s, 1))
+												   .reduceByKey((Integer i1, Integer i2) -> i1 + i2);
 
 		System.out.println("5-----------------------------");
 		List<Tuple2<String, Integer>> output = counts.collect();
@@ -60,4 +38,4 @@ public class WordCount {
 	}
 
 }
-//-Dspark.master=local
+// -Dspark.master=local
