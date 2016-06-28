9456efb30ec2361135f4c3ae3eea2f2e44cc419a
Jpz <Jiangpeizhao@outlook.com>
2016-06-28 14:51:12 +0800

    change

--- src/main/java/SparkLearn/learningSpark/FirstPairFunction.java
+++ src/main/java/SparkLearn/learningSpark/FirstPairFunction.java
@@ -21,12 +21,20 @@ public class FirstPairFunction {
 		SparkConf sc = new SparkConf().setAppName("FirstPairFunction");
 		JavaSparkContext jsc = new JavaSparkContext(sc);
 		JavaRDD<String> lines = jsc.textFile("D:\\behavior.txt", 1);
+		System.out.println(lines.toDebugString());
 		
 		JavaPairRDD<String, String> pairs = lines.mapToPair(x -> new Tuple2<String, String>(x.split("data")[0], x));
 		System.out.println(StringUtils.join(pairs.collect(), "\n"));
 		
 		JavaPairRDD<String, String> filter = pairs.filter(x -> x._2.contains("VIDE1327053569000253"));
+		System.out.println(filter.toDebugString());
 		System.out.println(StringUtils.join(filter.collect(), "\n"));
+		System.out.println(lines.getNumPartitions());
+		try {
+			Thread.sleep(1000000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		jsc.close();
 	}
 }
